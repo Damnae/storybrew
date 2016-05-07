5193e8da9058999158846311508d0d65be813a0a
Damnae <damnaee@gmail.com>
2016-05-07 21:07:38 +0200

    Improve the spectrum example.

--- scripts/Spectrum.cs
+++ scripts/Spectrum.cs
@@ -14,43 +14,46 @@ namespace StorybrewScripts
     {
         public override void Generate()
         {
-            MakeFft(0, 10000, "sb/pl.png");
+            // Create a spectrum effect from 0 to 20 seconds, using the sprite located at sb/pl.png
+            // 256 bars are created and updated every 1/8
+            MakeSpectrum(0, 10000, "sb/pl.png", 256, 8);
         }
 
-        private void MakeFft(int tStart, int tEnd, string spritePath)
+        private void MakeSpectrum(int tStart, int tEnd, string spritePath, int barCount, double beatDivisor)
         {
-            var layer = GetLayer("fft");
-            var fftTimeStep = Beatmap.GetTimingPointAt(tStart).BeatDuration / 8;
+            var layer = GetLayer("Spectrum");
+            var fftTimeStep = Beatmap.GetTimingPointAt(tStart).BeatDuration / beatDivisor;
 
-            var baseBars = new OsbSprite[256];
-            var barWidth = 640.0 / baseBars.Length;
+            var bars = new OsbSprite[barCount];
+            var barWidth = 640.0 / bars.Length;
             var imageWidth = 76;
 
-            for (var i = 0; i < baseBars.Length; i++)
+            for (var i = 0; i < bars.Length; i++)
             {
-                baseBars[i] = layer.CreateSprite(spritePath, OsbLayer.Background, OsbOrigin.CentreLeft, new Vector2());
-                baseBars[i].Move(tStart, i * barWidth, 240);
-                baseBars[i].Additive(tStart, tEnd);
+                bars[i] = layer.CreateSprite(spritePath, OsbLayer.Background, OsbOrigin.CentreLeft);
+                bars[i].Move(tStart, i * barWidth, 240);
+                bars[i].ScaleVec(tStart, barWidth / imageWidth, 0);
+                bars[i].Additive(tStart, tEnd);
             }
 
             for (var time = (double)tStart; time < tEnd; time += fftTimeStep)
             {
-                var fft = GetFft(time + fftTimeStep * 0.2, baseBars.Length);
-                for (var i = 0; i < baseBars.Length; i++)
+                var fft = GetFft(time + fftTimeStep * 0.2, bars.Length);
+                for (var i = 0; i < bars.Length; i++)
                 {
                     var height = fft[i] * 2000 / imageWidth;
                     if (height < 0.01) height = 0;
 
-                    var previousHeight = baseBars[i].ScaleAt(time).Y;
+                    var previousHeight = bars[i].ScaleAt(time).Y;
                     if (height == previousHeight)
                         continue;
 
                     if (height < previousHeight)
-                        baseBars[i].ScaleVec(time, time + fftTimeStep,
+                        bars[i].ScaleVec(time, time + fftTimeStep,
                             barWidth / imageWidth, previousHeight,
                             barWidth / imageWidth, height);
-                    else
-                        baseBars[i].ScaleVec(time,
+                    else if (height != previousHeight)
+                        bars[i].ScaleVec(time,
                             barWidth / imageWidth, height);
                 }
             }
