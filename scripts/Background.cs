96528d79eececf00651d49c930c85be35f5cc6f4
Damnae <damnaee@gmail.com>
2016-10-30 16:57:39 +0100

    Add a simple background script.

---
+++ scripts/Background.cs
@@ -0,0 +1,34 @@
+﻿using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using System.Linq;
+
+namespace StorybrewScripts
+{
+    public class Background : StoryboardObjectGenerator
+    {
+        [Configurable]
+        public string BackgroundPath = "bg.jpg";
+
+        [Configurable]
+        public int StartTime = 0;
+
+        [Configurable]
+        public int EndTime = 0;
+
+        [Configurable]
+        public double Opacity = 0.2;
+
+        public override void Generate()
+        {
+            var bitmap = GetMapsetBitmap(BackgroundPath);
+
+            if (StartTime == EndTime)
+                EndTime = (int)Beatmap.HitObjects.Last().EndTime;
+
+            var bg = GetLayer("").CreateSprite(BackgroundPath, OsbOrigin.Centre);
+            bg.Scale(StartTime, 480.0f / bitmap.Height);
+            bg.Fade(StartTime - 500, StartTime, 0, Opacity);
+            bg.Fade(EndTime, EndTime + 500, Opacity, 0);
+        }
+    }
+}
