aa05217a42e6ce1db7951712a6e6137e7b7407c6
Darky1 <Michele.Butkus@gmx.de>
2017-02-18 17:40:30 +0100

    Add support for .sbv subtitle files
    .sbv files are subtitle files created/used by youtube
    
    Format:
    0:00:11.920,0:00:14.240
    Start and Endtime separated by a comma
    
    0:00:17.480,0:00:18.720
    New lines separated with an empty line

---
+++ common/Subtitles/SbvParser.cs
@@ -0,0 +1,45 @@
+﻿using StorybrewCommon.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StorybrewCommon.Subtitles
+{
+    public class SbvParser
+    {
+        public SubtitleSet Parse (String path) 
+        {
+            string[] rawLines = System.IO.File.ReadAllLines(path);
+            List<SubtitleLine> lines = new List<SubtitleLine>();
+
+            double sTime = 0;
+            double eTime = 0;
+            String text = "";
+            foreach (String line in rawLines) 
+            {
+                String[] times = line.Split(','); //Get timing Strings.
+                if(times.Length == 2 && !line.Contains(" ")) //Is line a blockstart?
+                { 
+                    if(times[0].Split(':').Length == 3 && times[1].Split(':').Length == 3) //Make sure that this is a blockstart!
+                    { 
+                        if(text != "") //Had the previous block content?
+                        { 
+                            lines.Add(new SubtitleLine(sTime, eTime, text)); //Apply previous block
+                            text = ""; //Reset Text
+                        }
+                        
+                        continue;
+                    }
+                }
+                text += line; //Add line to blocktext
+            }
+            if(text != "") lines.Add(new SubtitleLine(sTime, eTime, text)); //Add last block
+            return new SubtitleSet(lines);
+        }
+
+        private double parseTimestamp(string timestamp)
+            => TimeSpan.Parse(timestamp).TotalMilliseconds;
+    }
+}
