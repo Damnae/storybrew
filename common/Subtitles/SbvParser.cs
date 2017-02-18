17944f5eaa0bc39dbf84435978f10fe39a3ee977
Darky1 <Michele.Butkus@gmx.de>
2017-02-18 22:58:34 +0100

    Cleaning up code
    Cleaned up everything that was pointed out

--- common/Subtitles/SbvParser.cs
+++ common/Subtitles/SbvParser.cs
@@ -1,43 +1,42 @@
-﻿using StorybrewCommon.Util;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace StorybrewCommon.Subtitles
 {
     public class SbvParser
     {
-        public SubtitleSet Parse (String path) 
+        public SubtitleSet Parse(String path)
         {
-            string[] rawLines = System.IO.File.ReadAllLines(path);
+            string[] rawLines = File.ReadAllLines(path);
             List<SubtitleLine> lines = new List<SubtitleLine>();
 
-            double sTime = 0;
-            double eTime = 0;
-            String text = "";
-            foreach (String line in rawLines) 
+            var startTime = 0d;
+            var endTime = 0d;
+            var text = "";
+
+            foreach (var line in rawLines)
             {
-                String[] times = line.Split(','); //Get timing Strings.
-                if(times.Length == 2 && !line.Contains(" ")) //Is line a blockstart?
-                { 
-                    if(times[0].Split(':').Length == 3 && times[1].Split(':').Length == 3) //Make sure that this is a blockstart!
-                    { 
-                        if(text != "") //Had the previous block content?
-                        { 
-                            lines.Add(new SubtitleLine(sTime, eTime, text)); //Apply previous block
-                            text = ""; //Reset Text
+                String[] times = line.Split(',');
+                if (times.Length == 2 && !line.Contains(" "))
+                {
+                    if (times[0].Split(':').Length == 3 && times[1].Split(':').Length == 3) //Make sure that this line is a blockstart!
+                    {
+                        if (text != "") //Had the previous block content?
+                        {
+                            lines.Add(new SubtitleLine(startTime, endTime, text));
+                            text = "";
                         }
-                        
-                        sTime = parseTimestamp(times[0]); //Parse timestamp for StartTime
-                        eTime = parseTimestamp(times[1]); //Parse timestamp for EndTime
+
+                        startTime = parseTimestamp(times[0]);
+                        endTime = parseTimestamp(times[1]);
+                        Console.WriteLine(startTime + "");
                         continue;
                     }
                 }
-                text += line; //Add line to blocktext
+                text += line;
             }
-            if(text != "") lines.Add(new SubtitleLine(sTime, eTime, text)); //Add last block
+            if (text != "") lines.Add(new SubtitleLine(startTime, endTime, text));
             return new SubtitleSet(lines);
         }
 
