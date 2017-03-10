cd8a79bb75a2d905b41229f5daccd232a8e5d242
Damnae <damnaee@gmail.com>
2017-03-11 00:27:10 +0100

    Rename IterativeSprite to CommandGenerator.

--- common/Storyboarding/Util/CommandGenerator.cs
+++ common/Storyboarding/Util/CommandGenerator.cs
@@ -1,13 +1,15 @@
 ﻿using OpenTK;
 using StorybrewCommon.Animations;
+using StorybrewCommon.Mapset;
 using StorybrewCommon.Storyboarding.CommandValues;
+using System;
 using System.Collections.Generic;
 
-namespace StorybrewCommon.Storyboarding
+namespace StorybrewCommon.Storyboarding.Util
 {
-    public class IterativeSprite
+    public class CommandGenerator
     {
-        private List<IterativeSpriteState> states = new List<IterativeSpriteState>();
+        private List<State> states = new List<State>();
 
         private readonly KeyframedValue<Vector2> positions = new KeyframedValue<Vector2>(InterpolatingFunctions.Vector2);
         private readonly KeyframedValue<Vector2> scales = new KeyframedValue<Vector2>(InterpolatingFunctions.Vector2);
@@ -21,8 +23,8 @@ namespace StorybrewCommon.Storyboarding
         private readonly KeyframedValue<CommandColor> finalColors = new KeyframedValue<CommandColor>(InterpolatingFunctions.CommandColor);
         private readonly KeyframedValue<float> finalOpacities = new KeyframedValue<float>(InterpolatingFunctions.Float);
 
-        public IterativeSpriteState StartState => states.Count == 0 ? null : states[0];
-        public IterativeSpriteState EndState => states.Count == 0 ? null : states[states.Count - 1];
+        public State StartState => states.Count == 0 ? null : states[0];
+        public State EndState => states.Count == 0 ? null : states[states.Count - 1];
 
         public double PositionTolerance = 1;
         public double ScaleTolerance = 0.1;
@@ -30,7 +32,7 @@ namespace StorybrewCommon.Storyboarding
         public double ColorTolerance = 1f / 255;
         public double OpacityTolerance = 0.01;
 
-        public void Add(IterativeSpriteState state)
+        public void Add(State state)
         {
             if (states.Count == 0 || states[states.Count - 1].Time < state.Time)
                 states.Add(state);
@@ -44,7 +46,7 @@ namespace StorybrewCommon.Storyboarding
 
         public void GenerateCommands(OsbSprite sprite, int width, int height)
         {
-            var previousState = (IterativeSpriteState)null;
+            var previousState = (State)null;
             var wasVisible = false;
             var stateAdded = false;
 
@@ -97,7 +99,7 @@ namespace StorybrewCommon.Storyboarding
             colors.TransferKeyframes(finalColors);
 
             opacities.Simplify1dKeyframes(OpacityTolerance, o => o);
-            opacities.Add(opacities.EndTime, 0);
+            if (opacities.EndValue > 0) opacities.Add(opacities.EndTime, 0);
             opacities.TransferKeyframes(finalOpacities);
         }
 
@@ -116,7 +118,7 @@ namespace StorybrewCommon.Storyboarding
             finalOpacities.Clear();
         }
 
-        private void addKeyframes(IterativeSpriteState state)
+        private void addKeyframes(State state)
         {
             var time = state.Time;
             positions.Add(time, state.Position);
@@ -124,6 +126,36 @@ namespace StorybrewCommon.Storyboarding
             rotations.Add(time, (float)state.Rotation);
             colors.Add(time, state.Color);
             opacities.Add(time, (float)state.Opacity);
+        }
+
+        public class State : IComparable<State>
+        {
+            public double Time;
+            public Vector2 Position = new Vector2(320, 240);
+            public Vector2 Scale = new Vector2(1, 1);
+            public double Rotation = 0;
+            public CommandColor Color = CommandColor.White;
+            public double Opacity = 1;
+
+            public bool IsVisible(int width, int height)
+            {
+                if (Opacity <= 0)
+                    return false;
+
+                if (Scale.X == 0 || Scale.Y == 0)
+                    return false;
+
+                var radius = Math.Max(width * Scale.X, height * Scale.Y) * 0.5 * Math.Sqrt(2);
+                var bounds = OsuHitObject.WidescreenStoryboardBounds;
+                if (Position.X + radius < bounds.Left || bounds.Right < Position.X - radius ||
+                    Position.Y + radius < bounds.Top || bounds.Bottom < Position.Y - radius)
+                    return false;
+
+                return true;
+            }
+
+            public int CompareTo(State other)
+                => Math.Sign(Time - other.Time);
         }
     }
 }
