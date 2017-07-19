d7cb0966d4e331c766212226e79efd7ae6141adb
Damnae <damnaee@gmail.com>
2017-07-19 17:55:49 +0200

    Add a KeyframedValue extension for flag commands.

---
+++ common/Animations/KeyframedValueExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StorybrewCommon.Animations
+{
+    public static class KeyframedValueExtensions
+    {
+        public static void ForEachFlag(this KeyframedValue<bool> keyframes, Action<double, double> action)
+        {
+            var active = false;
+            var startTime = 0.0;
+            var lastKeyframeTime = 0.0;
+            foreach (var keyframe in keyframes)
+                if (keyframe.Value != active)
+                {
+                    if (keyframe.Value)
+                    {
+                        startTime = keyframe.Time;
+                        active = true;
+                    }
+                    else
+                    {
+                        action(startTime, keyframe.Time);
+                        active = false;
+                    }
+                }
+                else lastKeyframeTime = keyframe.Time;
+
+            if (active)
+                action(startTime, lastKeyframeTime);
+        }
+
+    }
+}
