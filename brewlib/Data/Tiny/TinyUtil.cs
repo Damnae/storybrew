bc675bffb800f6b030247bcdf7a4ab30ce436df5
Damnae <damnaee@gmail.com>
2019-02-27 11:59:15 +0100

    Fix escaping/unescaping of strings in project files.

--- brewlib/Data/Tiny/TinyUtil.cs
+++ brewlib/Data/Tiny/TinyUtil.cs
@@ -1,19 +1,46 @@
-﻿namespace BrewLib.Data.Tiny
+﻿using System.Text;
+
+namespace BrewLib.Data.Tiny
 {
     public class TinyUtil
     {
         public static string EscapeString(string value)
-            => value?
-                .Replace("\\", "\\\\")
-                .Replace("\"", "\\\"")
-                .Replace("\r", "\\r")
-                .Replace("\n", "\\n");
+        {
+            var sb = new StringBuilder((int)(value.Length * 1.3f));
+            foreach (var c in value)
+                switch (c)
+                {
+                    case '\r': sb.Append("\\r"); break;
+                    case '\n': sb.Append("\\n"); break;
+                    case '"': sb.Append("\\\""); break;
+                    case '\\': sb.Append("\\\\"); break;
+                    default: sb.Append(c); break;
+                }
+
+            return sb.ToString();
+        }
 
         public static string UnescapeString(string value)
-            => value?
-                .Replace("\\n", "\n")
-                .Replace("\\r", "\r")
-                .Replace("\\\"", "\"")
-                .Replace("\\\\", "\\");
+        {
+            var special = false;
+
+            var sb = new StringBuilder(value.Length);
+            foreach (var c in value)
+                if (special)
+                {
+                    switch (c)
+                    {
+                        case 'r': sb.Append('\r'); break;
+                        case 'n': sb.Append('\n'); break;
+                        default: sb.Append(c); break;
+                    }
+                    special = false;
+                }
+                else if (c == '\\')
+                    special = true;
+                else sb.Append(c);
+
+            return sb.ToString();
+        }
     }
 }
