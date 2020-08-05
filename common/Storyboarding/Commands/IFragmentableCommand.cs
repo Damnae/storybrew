115fca3a3e734cb6becf5a6df3719e09068c52af
Damnae <damnaee@gmail.com>
2020-08-05 09:35:40 +0200

    Fix some line endings.

--- common/Storyboarding/Commands/IFragmentableCommand.cs
+++ common/Storyboarding/Commands/IFragmentableCommand.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace StorybrewCommon.Storyboarding.Commands
 {
