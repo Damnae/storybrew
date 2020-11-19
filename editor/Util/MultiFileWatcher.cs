b4d8617f73f2e8307efdceb362fe732a455a617e
Damnae <damnaee@gmail.com>
2020-11-19 11:33:51 +0100

    Fix MultiFileWatcher not working when the parent folder of a watched file doesn't exist yet.

--- editor/Util/MultiFileWatcher.cs
+++ editor/Util/MultiFileWatcher.cs
@@ -8,6 +8,7 @@ namespace StorybrewEditor.Util
     public class MultiFileWatcher : IDisposable
     {
         private Dictionary<string, FileSystemWatcher> folderWatchers = new Dictionary<string, FileSystemWatcher>();
+        private Dictionary<string, FileSystemWatcher> recursiveFolderWatchers = new Dictionary<string, FileSystemWatcher>();
         private HashSet<string> watchedFilenames = new HashSet<string>();
         private readonly ThrottledActionScheduler scheduler = new ThrottledActionScheduler();
 
@@ -32,21 +33,57 @@ namespace StorybrewEditor.Util
                 watchedFilenames.Add(filename);
             }
 
-            if (!folderWatchers.TryGetValue(directoryPath, out FileSystemWatcher watcher))
+            if (Directory.Exists(directoryPath))
             {
-                folderWatchers.Add(directoryPath, watcher = new FileSystemWatcher()
+                // The folder containing the file to watch exists, 
+                // only watch that folder
+
+                if (!folderWatchers.TryGetValue(directoryPath, out FileSystemWatcher watcher))
                 {
-                    Path = directoryPath,
-                    IncludeSubdirectories = false,
-                });
-                watcher.Created += watcher_Changed;
-                watcher.Changed += watcher_Changed;
-                watcher.Renamed += watcher_Changed;
-                watcher.Error += (sender, e) => Trace.WriteLine($"Watcher error: {e.GetException()}");
-                watcher.EnableRaisingEvents = true;
-                Trace.WriteLine($"Watching folder: {directoryPath}");
+                    folderWatchers.Add(directoryPath, watcher = new FileSystemWatcher()
+                    {
+                        Path = directoryPath,
+                        IncludeSubdirectories = false,
+                    });
+                    watcher.Created += watcher_Changed;
+                    watcher.Changed += watcher_Changed;
+                    watcher.Renamed += watcher_Changed;
+                    watcher.Error += (sender, e) => Trace.WriteLine($"Watcher error: {e.GetException()}");
+                    watcher.EnableRaisingEvents = true;
+                    Trace.WriteLine($"Watching folder: {directoryPath}");
+                }
+                Trace.WriteLine($"Watching file: {filename}");
             }
-            Trace.WriteLine($"Watching file: {filename}");
+            else
+            {
+                // The folder containing the file to watch does not exist, 
+                // find a parent to watch subfolders from
+
+                var parentDirectory = Directory.GetParent(directoryPath);
+                while (parentDirectory != null && !parentDirectory.Exists)
+                    parentDirectory = parentDirectory.Parent;
+
+                if (parentDirectory != null && parentDirectory != parentDirectory.Root)
+                {
+                    var parentDirectoryPath = parentDirectory.ToString();
+                    
+                    if (!recursiveFolderWatchers.TryGetValue(parentDirectoryPath, out FileSystemWatcher watcher))
+                    {
+                        recursiveFolderWatchers.Add(parentDirectoryPath, watcher = new FileSystemWatcher()
+                        {
+                            Path = parentDirectoryPath,
+                            IncludeSubdirectories = true,
+                        });
+                        watcher.Created += watcher_Changed;
+                        watcher.Changed += watcher_Changed;
+                        watcher.Renamed += watcher_Changed;
+                        watcher.Error += (sender, e) => Trace.WriteLine($"Watcher error: {e.GetException()}");
+                        watcher.EnableRaisingEvents = true;
+                        Trace.WriteLine($"Watching folder and subfolders: {parentDirectoryPath}");
+                    }
+                }
+                else Trace.WriteLine($"Cannot watch file: {filename}, directory does not exist");
+            }
         }
 
         public void Clear()
@@ -54,6 +91,10 @@ namespace StorybrewEditor.Util
             foreach (var folderWatcher in folderWatchers.Values)
                 folderWatcher.Dispose();
             folderWatchers.Clear();
+
+            foreach (var folderWatcher in recursiveFolderWatchers.Values)
+                folderWatcher.Dispose();
+            recursiveFolderWatchers.Clear();
 
             lock (watchedFilenames)
                 watchedFilenames.Clear();
