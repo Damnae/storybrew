55a1d7491ab9a80a316358f8016f6890449e1216
Coppertine <37494321+Coppertine@users.noreply.github.com>
2022-09-12 01:42:58 +1000

    Add EmptyWidget.cs

---
+++ editor/UserInterface/EmptyWidget.cs
@@ -0,0 +1,10 @@
+﻿using BrewLib.UserInterface;
+
+namespace StorybrewEditor.UserInterface
+{
+    public class EmptyWidget : Widget
+    {
+        public EmptyWidget(WidgetManager manager) : base(manager)
+        { }
+    }
+}
