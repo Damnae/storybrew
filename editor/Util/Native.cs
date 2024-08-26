10a5c0cacf5ed42802f118de02120c2549c87480
minisbett <39670899+minisbett@users.noreply.github.com>
2024-08-27 00:00:42 +0200

    replace thread.abort with a winapi-baesd implementation

---
+++ editor/Util/Native.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StorybrewEditor.Util
+{
+    public static class Native
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool TerminateThread(IntPtr hThread, uint dwExitCode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenThread(int dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hObject);
+    }
+}
